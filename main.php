a5f82a3b7f98664bf75db892b4ab3fab686da479
Nika Darab <nikadarab@Xaviers-Air.attlocal.net>
2020-03-30 14:07:04 -0500

    logout

--- main.php
+++ main.php
@@ -29,6 +29,18 @@ if(!isset($_SESSION['name'])){
 }
 else{
 ?>
+<?
+if(isset($_GET['logout'])){ 
+     
+     //Simple exit message
+     $fp = fopen("log.html", 'a');
+     fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
+     fclose($fp);
+      
+     session_destroy();
+     header("Location: index.php"); //Redirect the user
+ }
+ ?>
 <div id="wrapper">
     <div id="menu">
         <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
