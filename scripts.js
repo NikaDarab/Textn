d6e79a8fa69b38b891f45ac14aabb5eabe66b246
Nika Darab <nikadarab@Xaviers-Air.attlocal.net>
2020-04-04 17:28:54 -0500

    many changes

---
+++ scripts.js
@@ -0,0 +1,30 @@
+know = {
+    "hello" : "hi",
+    "how are you?" : "good",
+    "ok" : ":)"
+};
+
+let talk = () => {
+    let date = new Date().toLocaleString();
+    let msg = document.getElementById("input-text");
+    let list = document.getElementById("messages-list");
+    let question = document.createElement("li");
+    let questionText = document.createTextNode(msg.value +" "+ date);
+    question.classList.add("question");
+    question.appendChild(questionText);
+    list.appendChild(question);
+    if (know[msg.value]) {
+        let answer = document.createElement("li");
+        let answerText = document.createTextNode(know[msg.value] + " " + date);
+        answer.classList.add("response");
+        answer.appendChild(answerText);
+        list.appendChild(answer);
+    } else {
+        let idk = document.createElement("li");
+        let idkText = document.createTextNode('I don\'t know' + date);
+        idk.classList.add("response");
+        idk.appendChild(idkText);
+        list.appendChild(idk);
+    }
+    msg.value = "";
+};
