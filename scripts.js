ab1cbd69aa193f7851057d85680a10158114dcac
Nika Darab <nikadarab@Xaviers-Air.attlocal.net>
2020-04-05 18:00:40 -0500

    added more object values

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 know = {
     "hello" : "hi",
+    "hi": "hello",
     "how are you?" : "good",
+    "what's up?": "just coding, how about you?",
+    "I miss you!": "I miss you too!",
     "ok" : ":)"
 };
 
@@ -9,19 +12,19 @@ let talk = () => {
     let msg = document.getElementById("input-text");
     let list = document.getElementById("messages-list");
     let question = document.createElement("li");
-    let questionText = document.createTextNode(msg.value +"  "+ date);
+    let questionText = document.createTextNode(msg.value + "  " + date);
     question.classList.add("question");
     question.appendChild(questionText);
     list.appendChild(question);
     if (know[msg.value]) {
         let answer = document.createElement("li");
-        let answerText = document.createTextNode(know[msg.value]+"  "+date);
+        let answerText = document.createTextNode(know[msg.value] + "  " + date);
         answer.classList.add("response");
         answer.appendChild(answerText);
         list.appendChild(answer);
     } else {
         let idk = document.createElement("li");
-        let idkText = document.createTextNode('I don\'t know'+"  "+ date);
+        let idkText = document.createTextNode('I don\'t know' + "  " + date);
         idk.classList.add("response");
         idk.appendChild(idkText);
         list.appendChild(idk);
