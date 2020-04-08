de7fee59adfb448493010d0280539426ceb72069
Nika Darab <nikadarab@Xaviers-Air.attlocal.net>
2020-04-08 08:27:46 -0500

    testing again

--- scripts.js
+++ scripts.js
@@ -9,25 +9,25 @@ know = {
 
 let talk = () => {
     let date = new Date().toLocaleString();
-    let msg = document.getElementById("input-text");
-    let list = document.getElementById("messages-list");
+    let msg = $('#input-text')[0];
+    let list = $('#messages-list')[0];
     let question = document.createElement("li");
+    $(question).addClass("question");
     let questionText = document.createTextNode(msg.value + "  " + date);
-    question.classList.add("question");
-    question.appendChild(questionText);
-    list.appendChild(question);
+    $(question).append(questionText);
+     list.appendChild(question);
     if (know[msg.value]) {
         let answer = document.createElement("li");
         let answerText = document.createTextNode(know[msg.value] + "  " + date);
-        answer.classList.add("response");
-        answer.appendChild(answerText);
-        list.appendChild(answer);
+        $(answer).addClass("response");
+        $(answer).append(answerText);
+        $(list).append(answer);
     } else {
         let idk = document.createElement("li");
         let idkText = document.createTextNode('I don\'t know' + "  " + date);
-        idk.classList.add("response");
-        idk.appendChild(idkText);
-        list.appendChild(idk);
+        $(idk).addClass("response");
+        $(idk).append("I don't know"+ "  " + date);
+        $(list).append(idk);
     }
-    msg.value = "";
-};
+    $(msg.value) = "";
+}; 
